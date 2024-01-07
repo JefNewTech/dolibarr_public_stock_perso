af3be7ff1526835115b0005f6983825681600456
tw <tw@pretexx.fr>
2024-01-07 11:46:30 +0100

    #14 - Add prefix PS_ to everything in global namespace

--- public/index.php
+++ public/index.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-
 use artifaille\publicstock\model\Product;
 use artifaille\publicstock\dao\ProductDAO;
 use artifaille\publicstock\dao\CurrencyDAO;
@@ -19,7 +18,7 @@ $res = 0;
 
 // Try main.inc.php into web root known defined into CONTEXT_DOCUMENT_ROOT (not always defined)
 if (!$res && !empty($_SERVER["CONTEXT_DOCUMENT_ROOT"])) {
-    $res = @include($_SERVER["CONTEXT_DOCUMENT_ROOT"] . "/main.inc.php");
+    $res = @include $_SERVER["CONTEXT_DOCUMENT_ROOT"] . "/main.inc.php";
 }
 
 // Try main.inc.php into web root detected using web root calculated from SCRIPT_FILENAME
@@ -32,20 +31,20 @@ while ($i > 0 && $j > 0 && isset($tmp[$i]) && isset($tmp2[$j]) && $tmp[$i] == $t
     $j--;
 }
 if (!$res && $i > 0 && \file_exists(\substr($tmp, 0, ($i + 1)) . "/main.inc.php")) {
-    $res = @include(\substr($tmp, 0, ($i + 1)) . "/main.inc.php");
+    $res = @include \substr($tmp, 0, ($i + 1)) . "/main.inc.php";
 }
 if (!$res && $i > 0 && \file_exists(\dirname(\substr($tmp, 0, ($i + 1))) . "/main.inc.php")) {
-    $res = @include(\dirname(\substr($tmp, 0, ($i + 1))) . "/main.inc.php");
+    $res = @include \dirname(\substr($tmp, 0, ($i + 1))) . "/main.inc.php";
 }
 // Try main.inc.php using relative path
 if (!$res && \file_exists("../main.inc.php")) {
-    $res = @include("../main.inc.php");
+    $res = @include "../main.inc.php";
 }
 if (!$res && \file_exists("../../main.inc.php")) {
-    $res = @include("../../main.inc.php");
+    $res = @include "../../main.inc.php";
 }
 if (!$res && \file_exists("../../../main.inc.php")) {
-    $res = @include("../../../main.inc.php");
+    $res = @include "../../../main.inc.php";
 }
 if (!$res) {
     die("Include of main fails");
@@ -57,14 +56,17 @@ if (!$res) {
 // Load necessary Dolibarr globals
 global $conf, $db, $langs;
 
+$psTitle = $conf->global->PUBLICSTOCK_TITLE ?? 'Public stock';
+
 ?>
+
 <!doctype html>
 <head>
 <link rel="stylesheet" href="../css/publicstock.css">
-<title><?= $conf->global->PUBLICSTOCK_PAGE_TITLE ?></title>
+<title><?php echo $psTitle ?></title>
 </head>
 <body>
-	<h1><?= $conf->global->PUBLICSTOCK_PAGE_TITLE ?></h1>
+    <h1 class="ps_title"><?php echo $psTitle ?></h1>
 
 <?php
 // Make sure Products module is enabled
@@ -73,55 +75,57 @@ if (!\isModEnabled('product')) {
 }
 
 // Setup PSR-4 class autoload
-\spl_autoload_register(function ($className) {
-    $classNameWithoutRoot = \str_replace('artifaille\\publicstock\\', '', $className);
-    $filename = __DIR__ . DIRECTORY_SEPARATOR
+\spl_autoload_register(
+    function ($className) {
+        $classNameWithoutRoot = \str_replace('artifaille\\publicstock\\', '', $className);
+        $filename = __DIR__ . DIRECTORY_SEPARATOR
         . '..' . DIRECTORY_SEPARATOR
         . 'class' . DIRECTORY_SEPARATOR
         . \str_replace('\\', DIRECTORY_SEPARATOR, $classNameWithoutRoot) . '.php';
-    require_once $filename;
-});
+        include_once $filename;
+    }
+);
 
 
 // Read data
-$dao = new ProductDAO($db);
-$products = $dao->readProducts();
-$currencySymbol = $langs->getCurrencySymbol($conf->currency);
+$psDao = new ProductDAO($db);
+$psProducts = $psDao->readProducts();
+$psCurrencySymbol = $langs->getCurrencySymbol($conf->currency);
 
 // Display
-$content = '';
-if (empty($products)) {
-    $content .= $langs->translate('No product');
+$psContent = '';
+if (empty($psProducts)) {
+    $psContent .= $langs->translate('No product');
 } else {
-    foreach ($products as $product) {
-		if ($product->getImageName() === '') {
-			$imageBlock = '';
-		} else {
+    foreach ($psProducts as $product) {
+        if ($product->getImageName() === '') {
+            $imageBlock = '';
+        } else {
             $imageURL = (\defined('DOL_MAIN_URL_ROOT') ? (DOL_MAIN_URL_ROOT . '/') : '')
-				. 'document.php?modulepart=product&entity='
-            	. $product->getEntityId()
-            	. '&attachment=0&file='
-            	. $product->getReference() . '/'
-            	. $product->getImageName();
-			$imageBlock = <<<HTML
-				<img class="product_image" alt="product image" src="{$imageURL}">
+            . 'document.php?modulepart=product&entity='
+                . $product->getEntityId()
+                . '&attachment=0&file='
+                . $product->getReference() . '/'
+                . $product->getImageName();
+            $imageBlock = <<<HTML
+				<img class="ps_product_image" alt="Product image" src="{$imageURL}">
 HTML;
-		}
-        $content .= <<<HTML
-			<div class="product">
-				<h3 class="product_label">{$product->getLabel()}</h3>
-				<p class="product_price_stock">
-					{$product->getPrice()} {$currencySymbol} - {$langs->trans('Stock')} : {$product->getStock()}
+        }
+        $psContent .= <<<HTML
+			<div class="ps_product">
+				<h3 class="ps_product_label">{$product->getLabel()}</h3>
+				<p class="ps_product_price_stock">
+					{$product->getPrice()} {$psCurrencySymbol} - {$langs->trans('Stock')} : {$product->getStock()}
 				<p>
 				{$imageBlock}
-				<p class="product_desc">
+				<p class="ps_product_desc">
 					{$product->getDescription()}
 				</p>
         	</div>
 HTML;
     }
 }
-echo $content;
+echo $psContent;
 ?>
 
 </body>
